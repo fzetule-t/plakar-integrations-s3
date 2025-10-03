58d137fc81d0086576081fedd21be5e3e01e93b7
Antoine DHEYGERS <antoine.dheygers@cryptoweb.fr>
2025-10-03 09:32:51 +0200

    Fix symlink ownership preservation in FS exporter
    Use fileinfo.Type() == 'symlink' instead of bitwise operations on Mode(),
    since Type() returns a string indicating the file type. This ensures
    Lchown is properly called for symlinks when running with elevated privileges.

--- fs/exporter/fs.go
+++ fs/exporter/fs.go
@@ -86,7 +86,7 @@ func Lutimes(path string, atime time.Time, mtime time.Time) error {
 }
 
 func (p *FSExporter) SetPermissions(ctx context.Context, pathname string, fileinfo *objects.FileInfo) error {
-	if fileinfo.Mode()&os.ModeSymlink == 0 {
+	if fileinfo.Type() != "symlink" {
 		if err := os.Chmod(pathname, fileinfo.Mode()); err != nil {
 			return err
 		}
@@ -102,7 +102,7 @@ func (p *FSExporter) SetPermissions(ctx context.Context, pathname string, filein
 			}
 		}
 	}
-	if fileinfo.Mode()&os.ModeSymlink != 0 {
+	if fileinfo.Type() == "symlink" {
 		if err := Lutimes(pathname, fileinfo.ModTime(), fileinfo.ModTime()); err != nil {
 			return err
 		}
