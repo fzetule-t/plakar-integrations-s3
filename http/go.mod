99972c4f5ce723e46fa89f46b072e39c0a89fb63
Mathieu Masson <mathieu.masson@plakar.io>
2026-02-05 16:17:13 +0100

    Switch to the new httpd API.
    * While here get rid of the go-kloset-sdk dependency, this plugin is not
      meant to be run over RPC.

--- http/go.mod
+++ http/go.mod
@@ -2,30 +2,33 @@ module github.com/PlakarKorp/integration-http
 
 go 1.24.0
 
-toolchain go1.24.5
-
 require (
-	github.com/PlakarKorp/go-kloset-sdk v1.0.5
-	github.com/PlakarKorp/kloset v1.0.12
+	github.com/PlakarKorp/kloset v1.1.0-beta.1
 	github.com/google/uuid v1.6.0
 )
 
 require (
-	github.com/PlakarKorp/integration-grpc v1.0.18 // indirect
+	github.com/RaduBerinde/btreemap v0.0.0-20250419232817-bf0d809ae648 // indirect
+	github.com/cockroachdb/crlib v0.0.0-20250718215705-7ff5051265b9 // indirect
+	github.com/cockroachdb/errors v1.12.0 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20250429170803-42689b6311bb // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/getsentry/sentry-go v0.35.1 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.33 // indirect
+	github.com/minio/minlz v1.0.1 // indirect
 	github.com/nickball/go-aes-key-wrap v0.0.0-20170929221519-1c3aa3e4dfc5 // indirect
-	github.com/pierrec/lz4/v4 v4.1.22 // indirect
-	github.com/tink-crypto/tink-go/v2 v2.5.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.25 // indirect
+	github.com/prometheus/client_golang v1.23.0 // indirect
+	github.com/prometheus/procfs v0.17.0 // indirect
+	github.com/tink-crypto/tink-go/v2 v2.6.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/zeebo/blake3 v0.2.4 // indirect
-	golang.org/x/crypto v0.45.0 // indirect
-	golang.org/x/mod v0.30.0 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20251124214823-79d6a2a48846 // indirect
-	google.golang.org/grpc v1.77.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/exp v0.0.0-20250819193227-8b4c13bb791b // indirect
+	golang.org/x/mod v0.32.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 )
--- http/storage/client.go
+++ http/storage/client.go
@@ -17,7 +17,6 @@
 package storage
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -26,9 +25,9 @@ import (
 	"net/url"
 	"path"
 
-	network "github.com/PlakarKorp/integration-http/storage/contract"
+	"github.com/PlakarKorp/kloset/connectors/storage"
+	"github.com/PlakarKorp/kloset/location"
 	"github.com/PlakarKorp/kloset/objects"
-	"github.com/PlakarKorp/kloset/storage"
 )
 
 type Store struct {
@@ -55,61 +54,54 @@ func NewStore(ctx context.Context, proto string, storeConfig map[string]string) 
 	}, nil
 }
 
-func (s *Store) Location(ctx context.Context) (string, error) {
-	return s.location.String(), nil
+func (s *Store) Ping(ctx context.Context) error {
+	return nil
 }
 
-func (s *Store) sendRequest(method string, requestType string, payload any) (*http.Response, error) {
-	requestBody, err := json.Marshal(payload)
+func (s *Store) Origin() string        { return s.location.Host }
+func (s *Store) Root() string          { return s.location.Path }
+func (s *Store) Type() string          { return "http" }
+func (s *Store) Flags() location.Flags { return 0 }
+
+func (s *Store) sendRequest(method string, requestType string, payload io.Reader, rg *storage.Range) (*http.Response, error) {
+	u := *s.location
+	u.Path = path.Join(u.Path, requestType)
+	req, err := http.NewRequest(method, u.String(), payload)
 	if err != nil {
 		return nil, err
 	}
 
-	u := *s.location
-	u.Path = path.Join(u.Path, requestType)
-	req, err := http.NewRequest(method, u.String(), bytes.NewBuffer(requestBody))
-	if err != nil {
-		return nil, err
-	}
 	req.Header.Set("Content-Type", "application/json")
 	if s.authToken != "" {
 		req.Header.Set("Authorization", "Bearer "+s.authToken)
 	}
+	if rg != nil {
+		req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", rg.Offset, rg.Offset+uint64(rg.Length)))
+	}
+
 	return http.DefaultClient.Do(req)
 }
 
 func (s *Store) Create(ctx context.Context, config []byte) error {
-	var req network.ReqCreate
-	req.Configuration = config
-	r, err := s.sendRequest("POST", "/config", req)
-	if err != nil {
-		return err
-	}
-
-	var resCreate network.ResCreate
-	if err := json.NewDecoder(r.Body).Decode(&resCreate); err != nil {
-		return err
-	}
-	if resCreate.Err != "" {
-		return fmt.Errorf("%s", resCreate.Err)
-	}
 	return nil
 }
 
 func (s *Store) Open(ctx context.Context) ([]byte, error) {
-	r, err := s.sendRequest("GET", "/config", network.ReqOpen{})
+	r, err := s.sendRequest("GET", "/", nil, nil)
 	if err != nil {
 		return nil, err
 	}
+	defer r.Body.Close()
 
-	var resOpen network.ResOpen
-	if err := json.NewDecoder(r.Body).Decode(&resOpen); err != nil {
-		return nil, err
+	if r.StatusCode != 200 {
+		errmsg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("%s", errmsg)
 	}
-	if resOpen.Err != "" {
-		return nil, fmt.Errorf("%s", resOpen.Err)
-	}
-	return resOpen.Configuration, nil
+
+	return io.ReadAll(r.Body)
 }
 
 func (s *Store) Close(ctx context.Context) error {
@@ -124,260 +116,111 @@ func (s *Store) Size(ctx context.Context) (int64, error) {
 	return -1, nil
 }
 
-// states
-func (s *Store) GetStates(ctx context.Context) ([]objects.MAC, error) {
-	r, err := s.sendRequest("GET", "/states", network.ReqGetStates{})
+func (s *Store) List(ctx context.Context, res storage.StorageResource) ([]objects.MAC, error) {
+	uri := "/resources/" + strres(res)
+	r, err := s.sendRequest("GET", uri, nil, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var resGetStates network.ResGetStates
-	if err := json.NewDecoder(r.Body).Decode(&resGetStates); err != nil {
-		return nil, err
-	}
-	if resGetStates.Err != "" {
-		return nil, fmt.Errorf("%s", resGetStates.Err)
+	if r.StatusCode != 200 {
+		errmsg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("%s", errmsg)
 	}
 
-	return resGetStates.MACs, nil
+	var ret []objects.MAC
+	if err := json.NewDecoder(r.Body).Decode(&ret); err != nil {
+		return nil, err
+	} else {
+		return ret, nil
+	}
 }
 
-func (s *Store) PutState(ctx context.Context, MAC objects.MAC, rd io.Reader) (int64, error) {
-	data, err := io.ReadAll(rd)
+func (s *Store) Put(ctx context.Context, res storage.StorageResource, mac objects.MAC, rd io.Reader) (int64, error) {
+	uri := fmt.Sprintf("/resources/%s/%016x", strres(res), mac)
+	cr := &countingReader{rc: rd}
+	r, err := s.sendRequest("PUT", uri, cr, nil)
 	if err != nil {
-		return 0, err
+		return -1, err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != 200 {
+		errmsg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return -1, err
+		}
+		return -1, fmt.Errorf("%s", errmsg)
 	}
 
-	r, err := s.sendRequest("PUT", "/state", network.ReqPutState{
-		MAC:  MAC,
-		Data: data,
-	})
-	if err != nil {
-		return 0, err
-	}
-
-	var resPutState network.ResPutState
-	if err := json.NewDecoder(r.Body).Decode(&resPutState); err != nil {
-		return 0, err
-	}
-	if resPutState.Err != "" {
-		return 0, fmt.Errorf("%s", resPutState.Err)
-	}
-	return int64(len(data)), nil
+	return cr.n, nil
 }
 
-func (s *Store) GetState(ctx context.Context, MAC objects.MAC) (io.ReadCloser, error) {
-	r, err := s.sendRequest("GET", "/state", network.ReqGetState{
-		MAC: MAC,
-	})
+func (s *Store) Get(ctx context.Context, res storage.StorageResource, mac objects.MAC, rg *storage.Range) (io.ReadCloser, error) {
+	uri := fmt.Sprintf("/resources/%s/%016x", strres(res), mac)
+	r, err := s.sendRequest("GET", uri, nil, rg)
 	if err != nil {
 		return nil, err
 	}
 
-	var resGetState network.ResGetState
-	if err := json.NewDecoder(r.Body).Decode(&resGetState); err != nil {
-		return nil, err
+	if r.StatusCode != 200 {
+		errmsg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("%s", errmsg)
 	}
-	if resGetState.Err != "" {
-		return nil, fmt.Errorf("%s", resGetState.Err)
-	}
-	return io.NopCloser(bytes.NewBuffer(resGetState.Data)), nil
+	return r.Body, nil
 }
 
-func (s *Store) DeleteState(ctx context.Context, MAC objects.MAC) error {
-	r, err := s.sendRequest("DELETE", "/state", network.ReqDeleteState{
-		MAC: MAC,
-	})
+func (s *Store) Delete(ctx context.Context, res storage.StorageResource, mac objects.MAC) error {
+	uri := fmt.Sprintf("/resources/%s/%016x", strres(res), mac)
+	r, err := s.sendRequest("DELETE", uri, nil, nil)
 	if err != nil {
 		return err
 	}
+	defer r.Body.Close()
 
-	var resDeleteState network.ResDeleteState
-	if err := json.NewDecoder(r.Body).Decode(&resDeleteState); err != nil {
-		return err
-	}
-	if resDeleteState.Err != "" {
-		return fmt.Errorf("%s", resDeleteState.Err)
+	if r.StatusCode != 200 {
+		errmsg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("%s", errmsg)
 	}
 	return nil
 }
 
-// packfiles
-func (s *Store) GetPackfiles(ctx context.Context) ([]objects.MAC, error) {
-	r, err := s.sendRequest("GET", "/packfiles", network.ReqGetPackfiles{})
-	if err != nil {
-		return nil, err
+// we need a stringer on that enum
+func strres(s storage.StorageResource) string {
+	switch s {
+	case storage.StorageResourceUndefined:
+		return "undefined"
+	case storage.StorageResourcePackfile:
+		return "packfiles"
+	case storage.StorageResourceState:
+		return "states"
+	case storage.StorageResourceLock:
+		return "locks"
+	case storage.StorageResourceECCPackfile:
+		return "eccpackfiles"
+	case storage.StorageResourceECCState:
+		return "eccstates"
+	default:
+		return ""
 	}
-
-	var resGetPackfiles network.ResGetPackfiles
-	if err := json.NewDecoder(r.Body).Decode(&resGetPackfiles); err != nil {
-		return nil, err
-	}
-	if resGetPackfiles.Err != "" {
-		return nil, fmt.Errorf("%s", resGetPackfiles.Err)
-	}
-
-	return resGetPackfiles.MACs, nil
 }
 
-func (s *Store) PutPackfile(ctx context.Context, MAC objects.MAC, rd io.Reader) (int64, error) {
-	data, err := io.ReadAll(rd)
-	if err != nil {
-		return 0, err
-	}
-	r, err := s.sendRequest("PUT", "/packfile", network.ReqPutPackfile{
-		MAC:  MAC,
-		Data: data,
-	})
-	if err != nil {
-		return 0, err
-	}
-
-	var resPutPackfile network.ResPutPackfile
-	if err := json.NewDecoder(r.Body).Decode(&resPutPackfile); err != nil {
-		return 0, err
-	}
-	if resPutPackfile.Err != "" {
-		return 0, fmt.Errorf("%s", resPutPackfile.Err)
-	}
-	return int64(len(data)), nil
+type countingReader struct {
+	rc io.Reader
+	n  int64
 }
 
-func (s *Store) GetPackfile(ctx context.Context, MAC objects.MAC) (io.ReadCloser, error) {
-	r, err := s.sendRequest("GET", "/packfile", network.ReqGetPackfile{
-		MAC: MAC,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	var resGetPackfile network.ResGetPackfile
-	if err := json.NewDecoder(r.Body).Decode(&resGetPackfile); err != nil {
-		return nil, err
-	}
-	if resGetPackfile.Err != "" {
-		return nil, fmt.Errorf("%s", resGetPackfile.Err)
-	}
-	return io.NopCloser(bytes.NewBuffer(resGetPackfile.Data)), nil
-}
-
-func (s *Store) GetPackfileBlob(ctx context.Context, MAC objects.MAC, offset uint64, length uint32) (io.ReadCloser, error) {
-	r, err := s.sendRequest("GET", "/packfile/blob", network.ReqGetPackfileBlob{
-		MAC:    MAC,
-		Offset: offset,
-		Length: length,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	var resGetPackfileBlob network.ResGetPackfileBlob
-	if err := json.NewDecoder(r.Body).Decode(&resGetPackfileBlob); err != nil {
-		return nil, err
-	}
-	if resGetPackfileBlob.Err != "" {
-		return nil, fmt.Errorf("%s", resGetPackfileBlob.Err)
-	}
-	return io.NopCloser(bytes.NewBuffer(resGetPackfileBlob.Data)), nil
-}
-
-func (s *Store) DeletePackfile(ctx context.Context, MAC objects.MAC) error {
-	r, err := s.sendRequest("DELETE", "/packfile", network.ReqDeletePackfile{
-		MAC: MAC,
-	})
-	if err != nil {
-		return err
-	}
-
-	var resDeletePackfile network.ResDeletePackfile
-	if err := json.NewDecoder(r.Body).Decode(&resDeletePackfile); err != nil {
-		return err
-	}
-	if resDeletePackfile.Err != "" {
-		return fmt.Errorf("%s", resDeletePackfile.Err)
-	}
-	return nil
-}
-
-/* Locks */
-func (s *Store) GetLocks(ctx context.Context) ([]objects.MAC, error) {
-	r, err := s.sendRequest("GET", "/locks", &network.ReqGetLocks{})
-	if err != nil {
-		return []objects.MAC{}, err
-	}
-
-	var res network.ResGetLocks
-	if err := json.NewDecoder(r.Body).Decode(&res); err != nil {
-		return []objects.MAC{}, err
-	}
-	if res.Err != "" {
-		return []objects.MAC{}, fmt.Errorf("%s", res.Err)
-	}
-	return res.Locks, nil
-}
-
-func (s *Store) PutLock(ctx context.Context, lockID objects.MAC, rd io.Reader) (int64, error) {
-	data, err := io.ReadAll(rd)
-	if err != nil {
-		return 0, err
-	}
-
-	req := network.ReqPutLock{
-		Mac:  lockID,
-		Data: data,
-	}
-	r, err := s.sendRequest("PUT", "/lock", &req)
-	if err != nil {
-		return 0, err
-	}
-
-	var res network.ResPutLock
-	if err := json.NewDecoder(r.Body).Decode(&res); err != nil {
-		return 0, err
-	}
-	if res.Err != "" {
-		return 0, fmt.Errorf("%s", res.Err)
-	}
-	return int64(len(data)), nil
-}
-
-func (s *Store) GetLock(ctx context.Context, lockID objects.MAC) (io.ReadCloser, error) {
-	req := network.ReqGetLock{
-		Mac: lockID,
-	}
-	r, err := s.sendRequest("GET", "/lock", &req)
-	if err != nil {
-		return nil, err
-	}
-
-	var res network.ResGetLock
-	if err := json.NewDecoder(r.Body).Decode(&res); err != nil {
-		return nil, err
-	}
-
-	if res.Err != "" {
-		return nil, fmt.Errorf("%s", res.Err)
-	}
-
-	return io.NopCloser(bytes.NewReader(res.Data)), nil
-}
-
-func (s *Store) DeleteLock(ctx context.Context, lockID objects.MAC) error {
-	req := network.ReqDeleteLock{
-		Mac: lockID,
-	}
-	r, err := s.sendRequest("DELETE", "/lock", &req)
-	if err != nil {
-		return err
-	}
-
-	var res network.ResDeleteLock
-	if err := json.NewDecoder(r.Body).Decode(&res); err != nil {
-		return err
-	}
-
-	if res.Err != "" {
-		return fmt.Errorf("%s", res.Err)
-	}
-	return nil
+func (c *countingReader) Read(p []byte) (int, error) {
+	k, err := c.rc.Read(p)
+	c.n += int64(k)
+	return k, err
 }
