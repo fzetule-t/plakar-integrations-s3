aca56f1193d2c50814da4bf6206357dbe7b64632
Franck Z. <franck.zetule@pc-scol.fr>
2026-08-27 23:56:07 +0200

    small improvments

--- s3/importer/importer.go
+++ s3/importer/importer.go
@@ -235,6 +235,7 @@ func (p *S3Importer) Ping(ctx context.Context) error {
 
 func (p *S3Importer) Import(ctx context.Context, records chan<- *connectors.Record, results <-chan *connectors.Result) error {
 	defer close(records)
+	log.Printf("S3Exporter.Import")
 
 	const statWorkers = 16
 
@@ -248,7 +249,7 @@ func (p *S3Importer) Import(ctx context.Context, records chan<- *connectors.Reco
 	var workers sync.WaitGroup
 
 	// StatObject worker pool.
-	for i := 0; i < statWorkers; i++ {
+	for range statWorkers {
 		workers.Add(1)
 
 		go func() {
